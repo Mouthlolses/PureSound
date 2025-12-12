9029f4fe4242b13160a1c37b62bb61f9a0438a97
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-12-12 15:32:07 -0300

    start

--- app/src/main/java/com/mypuresound/puresound/MainActivity.kt
+++ app/src/main/java/com/mypuresound/puresound/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.ExperimentalMaterial3Api
+import com.mypuresound.puresound.ui.PureSoundApp
 import com.mypuresound.puresound.ui.theme.PureSoundTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { PureSoundTheme { } }
+        setContent { PureSoundTheme { PureSoundApp() } }
     }
 }
---
+++ app/src/main/java/com/mypuresound/puresound/ui/PureSoundApp.kt
@@ -0,0 +1,11 @@
+package com.mypuresound.puresound.ui
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun PureSoundApp() {
+
+
+
+}
